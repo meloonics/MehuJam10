14981f4ec803992d777a86d49c3f57c9526e6871
meloonics <59888410+meloonics@users.noreply.github.com>
2025-04-06 02:39:54 +0200

    nomnomnomnom omnonomnom

--- food.gd
+++ food.gd
@@ -3,11 +3,18 @@ extends Area2D
 @export_range(1, 50) var min_rats : int = 1
 
 @onready var radius : float = $CollisionShape2D.shape.radius
+var rats_left : int
+var progress : float = 0.0
+
+
 
 func _physics_process(delta: float) -> void:
 	var rats = get_overlapping_bodies()
+	rats_left = min_rats - rats.size()
+	%RatCount.visible = rats_left > 0 and min_rats > 1
+	%RatCount.set_text(str(rats_left))
 	if rats.size() >= min_rats:
-		for i in get_overlapping_bodies():
+		for i in rats:
 			if i is Rat:
 				var force : Vector2 = global_position - i.global_position
 				var dist : float = i.global_position.distance_to(global_position)
@@ -16,11 +23,42 @@ func _physics_process(delta: float) -> void:
 	
 	$CPUParticles2D.emitting = rats.size() >= min_rats
 	queue_redraw()
+	handle_progress(delta)
+	if progress > 0.0:
+		handle_shake()
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, 5, Color.RED)
-
+	if rats_left <= 0:
+		draw_circle(Vector2.ZERO, radius, Color.GREEN, false)
+	elif rats_left < min_rats:
+		draw_circle(Vector2.ZERO, radius, Color.RED, false)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Rat:
 		body.food_force = Vector2.ZERO
+
+func handle_progress(delta : float):
+	$ProgressBar.visible = rats_left <= 0
+	var deltabit = delta / (min_rats * 1.5)
+	if $ProgressBar.visible:
+		progress += deltabit + abs(rats_left) * deltabit
+		progress = min(1.0, progress)
+		$ProgressBar/nom.position.y = -$ProgressBar/nom.size.y * progress
+		$ProgressBar.value = progress
+	else:
+		progress = 0.0
+		$Sprite2D.offset = Vector2.ZERO
+	
+	if progress >= 1.0:
+		reward()
+
+func handle_shake() -> void:
+	
+	var min_shake : float = 1.0
+	var max_shake : float = 3.0
+	var shake : float = lerp(min_shake, max_shake, progress)
+	var offs = Vector2.RIGHT.rotated(randf() * TAU) * shake
+	$Sprite2D.offset = offs
+
+func reward() -> void:
+	queue_free()
